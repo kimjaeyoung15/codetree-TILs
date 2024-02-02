5c54e04d6fcdf21405f0ce5aa6104fefe19f5ff4
kimjaeyoung15 <151435248+kimjaeyoung15@users.noreply.github.com>
2024-02-02 16:35:03 +0900

    Title: 특정 조건 세 정수 비교, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240202/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,3 +1,3 @@
 nr=[int(x) for x in input().split()]
-print(1,end=' ') if nr[1]==min(nr) else print(0,end=' ')
-print(1) if nr[1]==nr[2] and nr[1]==nr[3] else print(0)
+print(1,end=' ') if nr[0]==min(nr) else print(0,end=' ')
+print(1) if nr[0]==nr[1] and nr[0]==nr[2] else print(0)
