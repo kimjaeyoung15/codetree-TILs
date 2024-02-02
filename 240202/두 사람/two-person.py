2b6bb9835b3dde915dfff145eff717208dccd31a
kimjaeyoung15 <151435248+kimjaeyoung15@users.noreply.github.com>
2024-02-02 17:00:06 +0900

    Title: 두 사람, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240202/두 사람/two-person.py
@@ -0,0 +1,7 @@
+p1a, p1s=input().split()
+p1a=int(p1a)
+p1s=str(p1s)
+p2a, p2s=input().split()
+p2a=int(p2a)
+p2s=str(p2s)
+print(1) if (p1a>=19 and p1s=='M') or (p2a>=19 and p2s=='M') else print(0)
