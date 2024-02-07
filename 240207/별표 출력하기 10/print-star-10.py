f5f3f86570685038e19154fed5a8c211e885150c
kimjaeyoung15 <151435248+kimjaeyoung15@users.noreply.github.com>
2024-02-07 20:39:55 +0900

    Title: 별표 출력하기 10, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,17 @@
+n=int(input())
+asc_arr=range(1,n+1)
+dsc_arr=range(n,0,-1)
+for i in range(n):
+    if i%2==0:
+        for x in range(asc_arr[i//2]):print('* ',end='')
+        print()
+    else:
+        for y in range(dsc_arr[i//2]):print('* ',end='')
+        print()
+for i in range(n-1,-1,-1):
+    if i%2==0:
+        for x in range(asc_arr[i//2]):print('* ',end='')
+        print()
+    else:
+        for y in range(dsc_arr[i//2]):print('* ',end='')
+        print()
