a73b8930a2d682a4420b7a5f8f08f0b74cda59ff
kimjaeyoung15 <151435248+kimjaeyoung15@users.noreply.github.com>
2024-02-26 19:37:37 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240226/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,12 @@
+a,b=map(int, input().split())
+divd,dvsr=a,b
+sumsq=0
+res=[0 for _ in range(9)]
+while True:
+    divd=divd//dvsr
+    res[divd%dvsr+1]+=1
+    if divd==0:
+        break
+for i in res:
+    sumsq+=i**2
+print(sumsq)
